f9b3409d303326ae2a988e632bc402d1bb7c0bcc
Jesse Barajas <jb19932010@yahoo.com>
2019-11-11 18:18:55 -0600

    Add 4th button

--- Sampler/Sampler.ino
+++ Sampler/Sampler.ino
@@ -4,19 +4,18 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 int velocity = 64;
 int channel = 1;
-int inputMemory[] = {0, 0, 0};
-int inputMemoryPitch[10] = {84, 85, 86};
+int inputMemory[] = {0, 0, 0, 0};
+int inputMemoryPitch[10] = {83, 84, 85, 86};
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode (2, INPUT_PULLUP);
   pinMode (3, INPUT_PULLUP);
   pinMode (4, INPUT_PULLUP);
+  pinMode (5, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int j = 2; j < 5; j++){
+  for (int j = 2; j < 6; j++){
     if (!digitalRead(j) == 1){
       if (inputMemory[j] == 0){
         MIDI.sendNoteOn(inputMemoryPitch[j], velocity, channel);
